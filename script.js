af3d8d6d3bd3f0b9e3553e9d8200f60a0c357b4f
Diuxx <nicolasmarmot@gmail.com>
2020-12-17 14:07:50 +0100

    pictures is correctly fetched

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 $(function () {
 
     fetchPictures('src/images.json');
-
-
-    // image list
-    let imgs = [
-        { src: 'src/hands-423794_640.jpg', title: 'Old man pipe', desc: 'wonderful' },
-        { src: 'src/noel-1897317_640.jpg', title: 'Secret agent', desc: 'beauty' },
-        { src: 'src/tea-783352_640.jpg', title: 'Travel poster Colorado', desc: 'nice' },
-        { src: 'src/still-life-851328_640.jpg', title: 'Travel poster National Parks', desc: 'gorgeous' },
-        { src: 'src/wood-cube-473703_640.jpg', title: 'We Can Do It', desc: 'sumptuous' },
-    ];
-
     let main = $('.main');
 
-    // display them all
-    /*imgs.forEach(img => {
-        main.append(`
-            <div class="img">
-                <img src="` + img.src + `" alt="` + img.title + `" width="200">
-                <div class="title">` + img.title + `</div>
-                <div class="desc">` + img.desc + `</div>
-            </div>
-        `);
-    });*/
-
-    // trigger onhover img
+    // trigger onhover
     $('.img').hover(
         function() {
             $(this).addClass('hover');
@@ -35,7 +13,6 @@ $(function () {
         }
     );
 
-
     /**
      * display all pics
      * @param {*} pics 
@@ -43,11 +20,9 @@ $(function () {
     function showPictures(data) {
         
         console.log(data) ;
-        console.log(data.picture) ;
         let pictures = data.pictures ;
 
         pictures.forEach(img => {
-            console.log(img) ;
             main.append(`
                 <div class="img">
                     <img src="` + img.src + `" alt="` + img.title + `" width="200">
